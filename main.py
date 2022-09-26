f38c60749ca325881c1e99287b5315703119eef9
sarahtell <sarah.lm.tell@gmail.com>
2022-09-26 19:42:18 +0200

    adds tests and functions for reading and parsing the data from log.txt

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-def read_file():
-    return None
+from datetime import datetime
+
+def strip_and_replace_in_line(line):
+    return [x.replace('\n', '').strip() for x in line]
+
+
+def remove_spaces_in_line(line):
+    return [x for x in line if x]
+
+
+def parse_line(line):
+    splitted_line = line.split("|")
+    stripped_line_replaced = strip_and_replace_in_line(splitted_line)
+    date_string, url, user_id = remove_spaces_in_line(stripped_line_replaced)
+    return [datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S%Z"), url, user_id]
+
+
+def read_file(file_name):
+    try:
+        with open(file_name) as f:
+            # i != 0 since we don't want to include the top row of log.txt. 
+            return [parse_line(line) for i, line in enumerate(f) if i != 0]
+    except FileNotFoundError as e:
+        e.strerror = "File does not exist!"
+        raise e
 
 def main():
+    logs = read_file("log.txt") # => [[],[],...,[]]
     pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- test/test_tracking.py
+++ test/test_tracking.py
@@ -1,5 +1,29 @@
-from main import read_file
+from main import read_file, parse_line
+from datetime import datetime
+import pytest
+
+
+FIRST_ROW = [
+        datetime.strptime("2019-03-01 09:00:00", "%Y-%m-%d %H:%M:%S"),
+        "/contact.html",
+        "12345"
+    ]
+
 
 def test_read_file():
-    file_content = read_file()
-    assert file_content == []
+    file_name = "log.txt"
+    file_name_with_path = "test/" + file_name
+    
+    with pytest.raises(FileNotFoundError) as excinfo:
+        read_file(file_name)
+
+    file_content = read_file(file_name_with_path)
+
+    assert "File does not exist!" in str(excinfo.value)
+    assert file_content[0] == FIRST_ROW
+
+
+def test_parse_line():
+    line = "|2019-03-01 09:00:00UTC |/contact.html |12345 |"
+    parsed_line = parse_line(line)
+    assert parsed_line == FIRST_ROW
