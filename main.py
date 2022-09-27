9fec60ef848811fc247f13d207a895c0a39832cf
sarahtell <sarah.lm.tell@gmail.com>
2022-09-27 22:12:01 +0200

    adds user_input + some refactoring

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from datetime import datetime
 from tracker import Tracker
+import argparse
+
+
+def set_time_format(date_string):
+    return datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S%Z")
+
 
 def strip_and_replace_in_line(line):
     return [x.replace("\n", "").strip() for x in line]
@@ -13,7 +19,7 @@ def parse_line(line):
     splitted_line = line.split("|")
     stripped_line_replaced = strip_and_replace_in_line(splitted_line)
     date_string, url, user_id = remove_spaces_in_line(stripped_line_replaced)
-    return [datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S%Z"), url, user_id]
+    return [set_time_format(date_string), url, user_id]
 
 
 def read_file(file_name):
@@ -21,15 +27,45 @@ def read_file(file_name):
         with open(file_name) as f:
             # i != 0 since we don't want to include the top row of log.txt.
             return [parse_line(line) for i, line in enumerate(f) if i != 0]
-    except FileNotFoundError as e:
-        e.strerror = "File does not exist!"
-        raise e
+    except FileNotFoundError:
+        print("File does not exist!")
+        exit()
+
+
+def get_user_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--startdate",
+        required=True,
+        help="input the start date of the time range on format YYYY-MM-DD HH:MM:SS",
+    )
+    parser.add_argument(
+        "--enddate",
+        required=True,
+        help="input the end date of the time range on format YYYY-MM-DD HH:MM:SS",
+    )
+    parser.add_argument(
+        "--filepath",
+        required=True,
+        help="input the path to the file containing the tracking data",
+    )
+
+    args = parser.parse_args()
+
+    return args.startdate, args.enddate, args.filepath
 
 
 def main():
-    logs = read_file("log.txt")
 
-    tracker = Tracker(logs, date_time_start=datetime(2019, 3, 1, 9, 0, 0), date_time_end=datetime(2019, 3, 2, 11, 59, 59))
+    startdate, enddate, filepath = get_user_input()
+
+    logs = read_file(filepath)
+
+    tracker = Tracker(
+        logs,
+        date_time_start=set_time_format(startdate),
+        date_time_end=set_time_format(enddate),
+    )
 
     date_time_parsed_log = tracker.filter_rows_by_date_range()
 
--- test/test_tracking.py
+++ test/test_tracking.py
@@ -4,22 +4,24 @@ import pytest
 
 
 FIRST_ROW = [
-        datetime.strptime("2019-03-01 09:00:00", "%Y-%m-%d %H:%M:%S"),
-        "/contact.html",
-        "12345"
-    ]
+    datetime.strptime("2019-03-01 09:00:00", "%Y-%m-%d %H:%M:%S"),
+    "/contact.html",
+    "12345",
+]
 
 
-def test_read_file():
+def test_read_file(capsys):
     file_that_does_not_exist = "random_file.txt"
     file_that_exists = "test/log.txt"
-    
-    with pytest.raises(FileNotFoundError) as excinfo:
+
+    with pytest.raises(SystemExit):
         read_file(file_that_does_not_exist)
 
+    out = capsys.readouterr()
+
     file_content = read_file(file_that_exists)
-    
-    assert "File does not exist!" in str(excinfo.value)
+
+    assert "File does not exist!" == out.out
     assert file_content[0] == FIRST_ROW
 
 
